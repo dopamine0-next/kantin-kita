415d12b24da99333096da8f31629b07ec9394184
Ardhan Novealdio <novealdioardhan@gmail.com>
2026-06-04 21:32:22 +0700

    connect voucher service ke backend /vouchers

--- frontend/src/services/promo/promo.service.ts
+++ frontend/src/services/promo/promo.service.ts
@@ -1,9 +1,9 @@
+import { fetcher } from '@/lib/fetcher'
+
 import { mapPromoItem } from './promo.mapper'
-import { MOCK_PROMO_API_RESPONSE } from './promo.mock'
-import { PromoItem } from './promo.types'
+import { PromoApiResponse, PromoItem } from './promo.types'
 
 export async function getPromos(): Promise<PromoItem[]> {
-  // Simulate network delay
-  await new Promise((resolve) => setTimeout(resolve, 500))
-  return MOCK_PROMO_API_RESPONSE.map(mapPromoItem)
+  const res = await fetcher<PromoApiResponse[]>('/promos')
+  return res.map(mapPromoItem)
 }
--- frontend/src/services/voucher/voucher.service.ts
+++ frontend/src/services/voucher/voucher.service.ts
@@ -1,9 +1,9 @@
+import { fetcher } from '@/lib/fetcher'
+
 import { mapVoucher } from './voucher.mapper'
-import { MOCK_VOUCHER_API_RESPONSE } from './voucher.mock'
-import { Voucher } from './voucher.types'
+import { Voucher, VoucherApiResponse } from './voucher.types'
 
 export async function getVouchers(): Promise<Voucher[]> {
-  // Simulate network delay
-  await new Promise((resolve) => setTimeout(resolve, 800))
-  return MOCK_VOUCHER_API_RESPONSE.map(mapVoucher)
+  const res = await fetcher<VoucherApiResponse[]>('/vouchers')
+  return res.map(mapVoucher)
 }
